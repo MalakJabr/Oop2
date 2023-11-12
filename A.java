e0b04372f9550394aa10eede6f9280de3d68bcc1
MalakJabr <malakjabe777@icloud.com>
2023-11-12 21:55:01 +0300

    class1

---
+++ A.java
@@ -0,0 +1,11 @@
+public class A implements Speaker {
+
+    public void hello() {
+        System.out.println("Hello from A, again -_-");
+    }
+
+    public void m1() {
+        System.out.println("Hello from A >_<");
+    }
+
+}
---
+++ C.java
@@ -0,0 +1,7 @@
+public class C extends B {
+
+    public void m1() {
+        System.out.println("Hello from C +_+");
+    }
+
+}
