5ac88c38ab59a47be53a1c67a1eb3cef9bd5d240
MalakJabr <135281551+MalakJabr@users.noreply.github.com>
2023-11-18 20:18:18 +0300

    Add files via upload

---
+++ week3_1oop2/src/pkg1/Oop1.java
@@ -0,0 +1,12 @@
+package pkg1;
+
+public class Oop1 {
+
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.name = "Malak";
+        p.sayHello();
+
+    }
+
+}
